e04b9e786643f835b96fd491156fd2b1927de888
Tanvin <tanvinwaseef15@gmail.com>
2025-11-28 13:06:38 +0600

    Ultimate final fix: Explicit pathing in craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,19 @@
+const path = require('path');
+
 module.exports = {
   style: {
     postcss: {
       plugins: [
-        require('tailwindcss'),
+        require('tailwindcss')(path.resolve(__dirname, 'tailwind.config.js')), // Explicit path
         require('autoprefixer'),
       ],
+    },
+  },
+  // You may also need to check for module resolution issues in Webpack:
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.extensions.push('.jsx');
+      return webpackConfig;
     },
   },
 };
